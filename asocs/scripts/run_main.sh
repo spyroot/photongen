8c672787929e82620c4b3493e07e728ebddfaace
spyroot <spyroot@gmail.com>
2022-01-12 04:58:51 +0400

    refactored

--- asocs/scripts/run_main.sh
+++ asocs/scripts/run_main.sh
@@ -3,20 +3,22 @@
 
 # mac os 
 # sudo launchctl limit maxfiles unlimited
+# will adjust max file descriptors
 #conda activate asocs
 export ANSIBLE_HOST_KEY_CHECKING=False
 export ANSIBLE_CONNECTION=local
 export ANSIBLE_INVENTORY=./hosts
 
+# install cisco nsxos
 ansible-galaxy collection install cisco.nxos
 
 # upgrade 
 #sudo pip install --ignore-installed --upgrade ansible
 
+# run linter first, check and run
 ansible-lint main.yml
 ansible-playbook main.yml --syntax-check
-
 ansible-playbook main.yml
 
-# debug mode
+# in debug mode , very chatty
 # ansible-playbook fabric.yml -vvvv
