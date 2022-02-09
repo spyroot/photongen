2c6cb8be2d6a325f1a2f6b965846ebbe1774909a
root <spyroot>
2022-02-09 02:15:58 +0000

    added DPDK 21.11 build

--- photonrt/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-hpagesize=1024
-
-mkdir -p /dev/hugepages
-mountpoint -q /dev/hugepages || mount -t hugetlbfs nodev /dev/hugepages
-echo $hpagesize > /sys/devices/system/node/node0/hugepages/hugepages-2048kB/nr_hugepages
-
