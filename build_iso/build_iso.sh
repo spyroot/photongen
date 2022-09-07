cc5d64d3b1e4637b35ce3af721bc5be485ac006d
root <root@epsilon01.cnfdemo.io>
2022-09-07 16:34:55 +0000

    iso builder

---
+++ build_iso/build_iso.sh
@@ -0,0 +1,53 @@
+apt-get install unzip
+apt install genisoimage jq -y
+sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+apt install golang-go
+sudo apt-get update && sudo apt-get install terraform
+
+wget -nc http://10.241.11.28/iso/photon/ph4-rt-refresh.iso
+
+umount /tmp/photon-iso
+rm -rf /tmp/photon-iso
+rm -rf /tmp/photon-ks-iso
+
+mkdir /tmp/photon-iso
+mount ph4-rt-refresh.iso /tmp/photon-iso
+
+mkdir /tmp/photon-ks-iso
+cp -r /tmp/photon-iso/* /tmp/photon-ks-iso/
+pushd /tmp/photon-ks-iso/
+
+cp isolinux/sample_ks.cfg isolinux/ks.cfg
+
+cat >> isolinux/menu.cfg << EOF
+label my_unattended
+	menu label ^My Unattended Install
+	menu default
+	kernel vmlinuz
+	append initrd=initrd.img root=/dev/ram0 ks=cdrom:/isolinux/ks.cfg loglevel=3 photon.media=cdrom
+EOF
+
+cat >> boot/grub2/grub.cfg << EOF
+set default=0
+set timeout=3
+loadfont ascii
+set gfxmode="1024x768"
+gfxpayload=keep
+set theme=/boot/grub2/themes/photon/theme.txt
+terminal_output gfxterm
+probe -s photondisk -u ($root)
+
+menuentry "Install" {
+    linux /isolinux/vmlinuz root=/dev/ram0 ks=cdrom:/isolinux/ks.cfg loglevel=3 photon.media=UUID=$photondisk
+    initrd /isolinux/initrd.img
+}
+EOF
+
+mkisofs -R -l -L -D -b isolinux/isolinux.bin -c isolinux/boot.cat \
+                -no-emul-boot -boot-load-size 4 -boot-info-table \
+                -eltorito-alt-boot --eltorito-boot boot/grub2/efiboot.img -no-emul-boot \
+                -V "PHOTON_$(date +%Y%m%d)" . > /home/vmware/build_iso/ph4-rt-refresh_adj.iso
+
+
+popd
+
