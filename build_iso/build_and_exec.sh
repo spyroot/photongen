658a66502bdfa8d7ab726d36db8c05411829cdc7
spyroot <spyroot@gmail.com>
2023-02-01 01:57:06 +0400

    Added additional docker load

--- build_iso/build_and_exec.sh
+++ build_iso/build_and_exec.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 # Pull reference iso.
 # Creates container and pull all requested packages.
-# Container will run buld_iso script and generate new iso file.
-# The new iso file generate to be a reference kick start unattended installer.
+# Container will run build_iso script and generate new iso file.
+# The new iso file generate to be a reference kick-start unattended installer.
 # Note: that docker run use current dir as volume make sure if you run on macos you
 # current dir added to resource.    Docker -> Preference -> Resource and add dir.
 # Author Mustafa Bayramov 
 
 # lint in case it has error.
 jsonlint ks.ref.cfg
+DEFAULT_ISO_LOCATION="https://drive.google.com/u/0/uc?id=101hVCV14ln0hkbjXZEI38L3FbcrvwUNB&export=download&confirm=1e-b"
+DEFAULT_IMAGE_NAME="ph4-rt-refresh_adj.iso"
 
-current_os=$(uname -a)
-if [[ $current_os == *"xnu"* ]]; then
-	brew_info_out=$(brew info wget | grep bottled)
-	if [[ $brew_info_out == *"vault: stable"* ]]; then
-		echo "wget already installed."
-	else
-		brew install wget
-	fi
-fi
-
+DEFAULT_HOSTNAME="photon-machine"
+DEFAULT_BOOT_SIZE="8192"
+DEFAULT_ROOT_SIZE="8192"
+#
+#current_os=$(uname -a)
+#if [[ $current_os == *"xnu"* ]]; then
+#	brew_info_out=$(brew info wget | grep bottled)
+#	if [[ $brew_info_out == *"vault: stable"* ]]; then
+#		echo "wget already installed."
+#	else
+#		brew install wget
+#	fi
+#fi
+#
 if [[ $current_os == *"linux"* ]]; then
 	apt-get update
 	apt-get install ca-certificates curl gnupg lsb-release python3-demjson
@@ -37,14 +43,65 @@ if [[ $current_os == *"linux"* ]]; then
 fi
 
 PUB_KEY=$HOME/.ssh/id_rsa.pub
+current_ks_phase="ks.ref.cfg"
 if test -f "$PUB_KEY"; then
-	export ssh_key=$(cat $HOME/.ssh/id_rsa.pub)
-	jq --arg key "$ssh_key" '.public_key = $key' ks.ref.cfg > ks.cfg 
-	jsonlint ks.cfg 
+	export ssh_key=$(cat "$HOME"/.ssh/id_rsa.pub)
+	jq --arg key "$ssh_key" '.public_key = $key' ks.ref.cfg > ks.phase1.cfg
+	current_ks_phase="ks.phase1.cfg"
+	jsonlint ks.phase1.cfg
+else
+  ssh-keygen
 fi
 
-wget -nc http://10.241.11.28/iso/photon/ph4-rt-refresh.iso
-docker rm -f /photon_iso_builder
-docker build -t spyroot/photon_iso_builder:1.0 .
-docker push spyroot/photon_iso_builder:1.0
-docker run --pull always -v `pwd`:`pwd` -w `pwd` --privileged --name photon_iso_builder --rm -i -t spyroot/photon_iso_builder:1.0 bash
+# read additional_packages and add required.
+ADDITIONAL=additional_packages.json
+[ ! -f $ADDITIONAL ] && { echo "$ADDITIONAL file not found"; exit 99; }
+packages=$(cat $ADDITIONAL)
+jq --argjson p "$packages" '.additional_packages += $p' $current_ks_phase > ks.phase2.cfg
+current_ks_phase="ks.phase2.cfg"
+jsonlint $current_ks_phase
+
+# adjust hostname
+jq --arg p "$DEFAULT_HOSTNAME" '.hostname=$p' $current_ks_phase > ks.phase3.cfg
+current_ks_phase="ks.phase3.cfg"
+jsonlint $current_ks_phase
+
+# adjust /root partition if needed
+jq --arg s "$DEFAULT_ROOT_SIZE" '.partitions[1].size=$s'  $current_ks_phase > ks.phase4.cfg
+current_ks_phase="ks.phase4.cfg"
+jsonlint $current_ks_phase
+
+# adjust /boot partition if needed
+jq --arg s "$DEFAULT_BOOT_SIZE" '.partitions[2].size=$s' $current_ks_phase > ks.phase5.cfg
+current_ks_phase="ks.phase5.cfg"
+jsonlint $current_ks_phase
+
+# adjust installation and add additional if needed.
+ADDITIONAL_RPMS=additional_direct_rpms.json
+[ ! -f $ADDITIONAL_RPMS ] && { echo "$ADDITIONAL_RPMS file not found"; exit 99; }
+rpms=$(cat $ADDITIONAL_RPMS)
+jq --argjson p "$rpms" '.postinstall += $p' $current_ks_phase > ks.phase6.cfg
+current_ks_phase="ks.phase6.cfg"
+jsonlint $current_ks_phase
+
+# additional docker load.
+DOCKER_LOAD_POST_INSTALL=additional_load_docker.json
+[ ! -f $DOCKER_LOAD_POST_INSTALL ] && { echo "$DOCKER_LOAD_POST_INSTALL file not found"; exit 99; }
+docker_imgs=$(cat $DOCKER_LOAD_POST_INSTALL)
+jq --argjson i "$docker_imgs" '.postinstall += $i' $current_ks_phase > ks.phase7.cfg
+current_ks_phase="ks.phase7.cfg"
+jsonlint $current_ks_phase
+
+# additional files that we copy from cdorom
+ADDITIONAL_FILES=additional_files.json
+[ ! -f $ADDITIONAL_FILES ] && { echo "$ADDITIONAL_FILES file not found"; exit 99; }
+additional_files=$(cat $ADDITIONAL_FILES)
+jq --argjson f "$additional_files" '. += $f' $current_ks_phase > ks.cfg
+current_ks_phase="ks.cfg"
+jsonlint $current_ks_phase
+
+rm ks.phase[0-9].cfg
+#wget -nc -O $DEFAULT_IMAGE_NAME "$DEFAULT_ISO_LOCATION"
+#docker rm -f /photon_iso_builder
+#docker build -t spyroot/photon_iso_builder:1.0 .
+#docker run --pull always -v `pwd`:`pwd` -w `pwd` --privileged --name photon_iso_builder --rm -i -t spyroot/photon_iso_builder:1.0 bash
--- build_iso/build_iso.sh
+++ build_iso/build_iso.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# unpack iso , put kickstart , docker image and repack.
+# Author Mustafa Bayramov
 
 current_os=$(uname -a)
 if [[ $current_os == *"xnu"* ]]; 
 then
-echo "You must run the script inside docker runtime."
+  echo "You must run the script inside docker runtime."
 exit 2
 fi
 
@@ -23,6 +25,7 @@ cp -r /tmp/photon-iso/* /tmp/photon-ks-iso/
 pushd /tmp/photon-ks-iso/
 cp $workspace_dir/ks.cfg isolinux/ks.cfg
 
+# generate isolinux
 cat > isolinux/isolinux.cfg << EOF
 include menu.cfg
 default vesamenu.c32
@@ -30,6 +33,7 @@ prompt 1
 timeout 1
 EOF
 
+# generate menu
 cat >> isolinux/menu.cfg << EOF
 label my_unattended
 	menu label ^Unattended Install
@@ -38,6 +42,7 @@ label my_unattended
 	append initrd=initrd.img root=/dev/ram0 ks=cdrom:/isolinux/ks.cfg loglevel=3 photon.media=cdrom
 EOF
 
+# generate grub
 cat > boot/grub2/grub.cfg << EOF
 set default=1
 set timeout=1
@@ -60,7 +65,7 @@ sed -i 's/default install/default my_unattended/g' /tmp/photon-ks-iso/isolinux/m
 mkisofs -R -l -L -D -b isolinux/isolinux.bin -c isolinux/boot.cat \
                 -no-emul-boot -boot-load-size 4 -boot-info-table \
                 -eltorito-alt-boot --eltorito-boot boot/grub2/efiboot.img -no-emul-boot \
-                -V "PHOTON_$(date +%Y%m%d)" . > $workspace_dir/ph4-rt-refresh_adj.iso
+                -V "PHOTON_$(date +%Y%m%d)" . > "$workspace_dir"/ph4-rt-refresh_adj.iso
 popd
 
 umount /tmp/photon-iso
