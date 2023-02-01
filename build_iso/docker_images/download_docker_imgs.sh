9aec71a969acbfd6211236034bc8cb16d1114458
spyroot <spyroot@gmail.com>
2023-02-01 15:44:57 +0400

    added clean and re-use image

--- build_iso/docker_images/download_docker_imgs.sh
+++ build_iso/docker_images/download_docker_imgs.sh
@@ -1,1 +1,9 @@
-scp vmware@$BUILD_HOST:/home/vmware/vcu.tar.gz vcu.tar.gz
+#!/bin/bash
+DEFAULT_IMAGE_NAME=vcu.tar.gz
+if [[ ! -v BUILD_HOST ]]; then
+    echo "BUILD_HOST is not set"
+elif [[ -z "$BUILD_HOST" ]]; then
+    echo "BUILD_HOST is set to the empty string"
+else
+  scp vmware@"$BUILD_HOST":/home/vmware/$DEFAULT_IMAGE_NAME $DEFAULT_IMAGE_NAME
+fi
