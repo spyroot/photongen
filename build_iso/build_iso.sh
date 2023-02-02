8733b87d56283987cba0c1c565d4230b486d1c17
spyroot <spyroot@gmail.com>
2023-02-02 10:36:15 +0400

    added parallel boot

--- build_iso/build_iso.sh
+++ build_iso/build_iso.sh
@@ -30,7 +30,7 @@ echo "Copy data from $DEFAULT_SRC_ISO_DIR/* to $DEFAULT_DST_ISO_DIR/"
 
 cp -r $DEFAULT_SRC_ISO_DIR/* $DEFAULT_DST_ISO_DIR/
 cp docker_images/*.tar.gz $DEFAULT_DST_ISO_DIR/
-cp post.sh cp post.sh $DEFAULT_DST_ISO_DIR/
+cp post.sh $DEFAULT_DST_ISO_DIR/
 
 pushd $DEFAULT_DST_ISO_DIR/ || exit
 cp "$workspace_dir"/ks.cfg isolinux/ks.cfg
