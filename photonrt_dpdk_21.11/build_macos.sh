3fb77685051104f9d5b842a82f90490f16c53212
spyroot <spyroot@gmail.com>
2024-02-08 00:46:17 +0400

    adjust 21.11

---
+++ photonrt_dpdk_21.11/build_macos.sh
@@ -0,0 +1,2 @@
+
+docker build --platform linux/amd64 -t cnfdemo.io/spyroot/dpdk_rt_dpdk21_tester:latest .
---
+++ photonrt_dpdk_21.11/build_x86.sh
@@ -0,0 +1,1 @@
+docker buildx build --platform linux/amd64 -t cnfdemo.io/spyroot/dpdk_rt_dpdk21_tester:latest .
