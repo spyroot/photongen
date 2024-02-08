8d87fc0c589b616760423ba524d0304192d63313
spyroot <spyroot@gmail.com>
2024-02-08 16:25:02 +0400

    packet gen tool box

--- pktgen_toolbox/scriipts/build_x86.sh
+++ pktgen_toolbox/scriipts/build_x86.sh
@@ -3,6 +3,6 @@ docker rmi cnfdemo.io/spyroot/pktgen_toolbox > build.generic.log 2>&1
 docker buildx build \
 	--platform linux/amd64 \
 	--build-arg MESON_ARGS="-Dplatform=native" \
-	-t cnfdemo.io/spyroot/pktgen_toolbox:latest .
+	-t cnfdemo.io/spyroot/pktgen_toolbox:latest ../
 
 docker tag cnfdemo.io/spyroot/pktgen_toolbox:latest spyroot/pktgen_toolbox:latest
