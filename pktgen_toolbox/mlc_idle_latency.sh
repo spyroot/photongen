6d19152cb1bb5ad3872cccf2faf55b76c58c31ce
spyroot <spyroot@gmail.com>
2024-02-09 02:06:05 +0400

    output json

--- pktgen_toolbox/mlc_idle_latency.sh
+++ pktgen_toolbox/mlc_idle_latency.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 # invoke mlc idle latency
-/root/mlc/Linux/mlc --idle_latency
+# buffer sizes
+BUFFER_SIZE=(16 32 64 128)
+# Loop through each buffer size
+for size in "${BUFFER_SIZE[@]}"
+do
+    /root/mlc/Linux/mlc --idle_latency -t10 -b"${size}"m | grep -oP '(?<=\().*(?=ns)' >> "output_idle_latency.txt"
+done
