8293c218344e9a9933b7dbce329d39b33cb72294
spyroot <spyroot@gmail.com>
2024-02-08 16:22:23 +0400

    added toolbox

--- pktgen_toolbox/build_x86.sh
+++ pktgen_toolbox/build_x86.sh
@@ -1,4 +1,8 @@
+docker rmi cnfdemo.io/spyroot/pktgen_toolbox > build.generic.log 2>&1
+
 docker buildx build \
 	--platform linux/amd64 \
 	--build-arg MESON_ARGS="-Dplatform=native" \
-	-t cnfdemo.io/spyroot/dpdk_pktgen_iperf_native:latest .
+	-t cnfdemo.io/spyroot/pktgen_toolbox:latest .
+
+docker tag cnfdemo.io/spyroot/pktgen_toolbox:latest spyroot/pktgen_toolbox:latest
---
+++ pktgen_toolbox/run_iperf_server.sh
@@ -0,0 +1,1 @@
+docker run -e MODE=server -e PORT=5001 -it --privileged --rm cnfdemo.io/spyroot/dpdk_pktgen_iperf_native /bin/bash
