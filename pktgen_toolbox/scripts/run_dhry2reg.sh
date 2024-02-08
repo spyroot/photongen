292fa80f526a3d1799f3ab13b5d1c4ee908da539
spyroot <spyroot@gmail.com>
2024-02-09 01:09:03 +0400

    output json

--- pktgen_toolbox/scripts/run_dhry2reg.sh
+++ pktgen_toolbox/scripts/run_dhry2reg.sh
@@ -1,1 +1,1 @@
-docker run -e MODE=server -e PORT=5001 -it --privileged --rm spyroot/pktgen_toolbox:latest /ub_dhry2reg.sh
+docker run -it --privileged --rm spyroot/pktgen_toolbox:latest /ub_dhry2reg.sh
---
+++ pktgen_toolbox/scripts/run_mlc_idle.sh
@@ -0,0 +1,1 @@
+docker run -it --privileged --rm spyroot/pktgen_toolbox:latest /mlc_idle_latency.sh
