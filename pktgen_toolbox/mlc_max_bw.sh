fb8526991110415f1cccac28462fd18b67fc81ac
spyroot <spyroot@gmail.com>
2024-02-09 04:12:49 +0400

    output json

--- pktgen_toolbox/mlc_max_bw.sh
+++ pktgen_toolbox/mlc_max_bw.sh
@@ -14,6 +14,9 @@
 #   STRIDE      - A space-separated list of stride sizes. The stride size
 #                 determines the gap between successive memory accesses.
 #                 Example: "8 16 24 32"
+#   cores       - A space-separated list of CPU cores on which the test should
+#                 be run. This controls the core affinity for the test.
+#                 Example: "0 1 2 3"
 #
 #   If these variables are not set, the script uses default values for buffer sizes,
 #   stride sizes, and will run the test without specific core affinity.
@@ -35,30 +38,42 @@
 # spyroot@gmail.com
 # Author Mustafa Bayramov
 
-> "/output_idle_latency.txt"
+OUTPUT_PATH_FILE="/output_max_bandwidth.txt"
+> "$OUTPUT_PATH_FILE"
 
 if [[ -n "$BUFFER_SIZE" ]]; then
-	IFS=' ' read -r -a BUFFER_SIZE <<< "$BUFFER_SIZE"
+    IFS=' ' read -r -a BUFFER_SIZE <<< "$BUFFER_SIZE"
 else
-	BUFFER_SIZE=(8 16 24 32 64 128 512 1024)
+    BUFFER_SIZE=(8 16 24 32 64 128 512 1024)
 fi
 
 if [[ -n "$STRIDE" ]]; then
-	IFS=' ' read -r -a STRIDE <<< "$STRIDE"
+    IFS=' ' read -r -a STRIDE <<< "$STRIDE"
 else
-	STRIDE=(8 16 24 32 64 128)
+    STRIDE=(8 16 24 32 64 128)
 fi
 
-echo "Running idle latency tests for buffer sizes: ${BUFFER_SIZE[*]} and strides: ${STRIDE[*]}"
-# collect for over each stride and buffer size
+if [[ -n "$cores" ]]; then
+    IFS=' ' read -r -a cores <<< "$cores"
+    cores_str="${cores[*]}"
+    if_per_cores="-k$cores_str"
+else
+    cores_str=""
+    if_per_cores=""
+fi
+
+echo "Running max bandwidth tests for buffer sizes: \
+${BUFFER_SIZE[*]} and strides: ${STRIDE[*]} with cores: ${cores_str}"
 for stride in "${STRIDE[@]}"
 do
-  for size in "${BUFFER_SIZE[@]}"
+  for bs in "${BUFFER_SIZE[@]}"
   do
-    echo "Running for buffer ${size}m stride ${stride}"
-    latency=$(/root/mlc/Linux/mlc --idle_latency -t10 -b"${size}"m -l"${stride}" | grep -oP '(?<=\().*(?=ns)' | sed 's/ //g') && \
-      echo "${latency}ns - ${size}MB - ${stride}" >> "/output_idle_latency.txt"
+    echo "Running for buffer ${bs}m stride ${stride} cores ${cores_str}"
+    mlc_command="/root/mlc/Linux/mlc --max_bandwidth -t1 -b${bs}m ${if_per_cores}"
+    rw=$(eval "$mlc_command" | awk -v bs="$bs" \
+    -v cores="$cores_str" '/ALL Reads/ {flag=1} flag {print "size " bs ", cores " cores ", " $0}')
+    echo "$rw" | awk '/size [0-9]+, cores [^,]+, .+:/ {print}' >> "$OUTPUT_PATH_FILE"
   done
 done
 
-cat /output_idle_latency.txt
+cat "$OUTPUT_PATH_FILE"
