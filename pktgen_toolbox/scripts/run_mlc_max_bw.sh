57ca5c185de4c9ae1932132d29f45aae51b896b2
spyroot <spyroot@gmail.com>
2024-02-09 04:40:15 +0400

    adjusted

--- pktgen_toolbox/scripts/run_mlc_max_bw.sh
+++ pktgen_toolbox/scripts/run_mlc_max_bw.sh
@@ -2,4 +2,4 @@ docker run \
 -e BUFFER_SIZE="8 16" \
 -e STRIDE="24 32" \
 -it --privileged --rm \
-spyroot/pktgen_toolbox:latest /bin/bash -c "/mlc_peak_inject_bw.sh"
+spyroot/pktgen_toolbox:latest /bin/bash -c "/mlc_max_bw.sh"
