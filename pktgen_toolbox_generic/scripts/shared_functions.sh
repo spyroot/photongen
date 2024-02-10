c6c77190f9e82f9e995e9d855036d2fda8b4dd27
spyroot <spyroot@gmail.com>
2024-02-11 02:09:08 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/shared_functions.sh
+++ pktgen_toolbox_generic/scripts/shared_functions.sh
@@ -10,10 +10,16 @@ function vf_mac_address() {
         return
     fi
 
-    if [[ $_pci_address =~ ^[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:[0-9a-fA-F]{1,2}$ ]]; then
+    # Check if the PCI address is in the format "03:02:.4" or "03:.4"
+    if [[ $_pci_address =~ ^([0-9a-fA-F]{2}:)?([0-9a-fA-F]{2}:)?[0-9a-fA-F]{2}\.[0-9a-fA-F]{1,2}$ ]]; then
         # Prepend "0000" to the PCI address
         _pci_address="0000:${_pci_address}"
+    elif [[ ! $_pci_address =~ ^([0-9a-fA-F]{4}:){3}[0-9a-fA-F]{2}\.[0-9a-fA-F]{1,2}$ ]]; then
+        # If the PCI address doesn't match any of the expected formats, print an error message and return
+        echo "Invalid PCI address format: $_pci_address"
+        return 1
     fi
+
 
     _mac_address=$(dmesg | grep "$_pci_address" | \
     grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
