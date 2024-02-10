2bc940d5f4091e066f1394ec2f2ae6216b414f11
spyroot <spyroot@gmail.com>
2024-02-10 13:34:35 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/select_vf.sh
+++ pktgen_toolbox_generic/scripts/select_vf.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+# Select a VF DPDK.
 
 DPDK_PMD_TYPE="vfio-pci"
+BUS_FILTER="$1"
 
-# select vf DPDK cable
 output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep $DPDK_PMD_TYPE | grep Virtual)"
 readarray -t pci_devices <<< "$(echo "$output" | awk '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
 
 for pci in "${pci_devices[@]}"; do
-	echo "$pci"
+    if [[ -n "$BUS_FILTER" ]]; then
+        if [[ "$pci" == "$BUS_FILTER"* ]]; then
+            echo "$pci"
+        fi
+    else
+        # If no bus filter is provided, show all VFs.
+        echo "$pci"
+    fi
 done
-
