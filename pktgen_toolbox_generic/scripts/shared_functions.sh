c2a73c19c02bb1867026d44ed0f715e81a7d2c7a
spyroot <spyroot@gmail.com>
2024-02-11 02:02:07 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/shared_functions.sh
+++ pktgen_toolbox_generic/scripts/shared_functions.sh
@@ -3,6 +3,13 @@
 function vf_mac_address() {
     local _pci_address=$1
     local _mac_address
+
+    if [ -z "$_pci_address" ]; then
+        # Return an empty string
+        echo ""
+        return
+    fi
+
     _mac_address=$(dmesg | grep "$_pci_address" | \
     grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
     echo "$_mac_address"
