3f32117d3610caa5ce0950d73f8e2981309ca166
spyroot <spyroot@gmail.com>
2024-02-10 13:36:09 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/select_vf.sh
+++ pktgen_toolbox_generic/scripts/select_vf.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-# Select a VF DPDK.
 
-DPDK_PMD_TYPE="vfio-pci"
-BUS_FILTER="$1"
+function select_vf_dpdk {
+    local DPDK_PMD_TYPE="vfio-pci"
+    local BUS_FILTER="$1"
 
-output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep $DPDK_PMD_TYPE | grep Virtual)"
-readarray -t pci_devices <<< "$(echo "$output" | awk '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
+    local output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep $DPDK_PMD_TYPE | grep Virtual)"
+    local pci_devices
+    readarray -t pci_devices <<< "$(echo "$output" | awk '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
 
-for pci in "${pci_devices[@]}"; do
-    if [[ -n "$BUS_FILTER" ]]; then
-        if [[ "$pci" == "$BUS_FILTER"* ]]; then
+    for pci in "${pci_devices[@]}"; do
+        if [[ -n "$BUS_FILTER" ]]; then
+            if [[ "$pci" == "$BUS_FILTER"* ]]; then
+                echo "$pci"
+            fi
+        else
+            # If no bus filter is provided, show all VFs.
             echo "$pci"
         fi
-    else
-        # If no bus filter is provided, show all VFs.
-        echo "$pci"
-    fi
-done
+    done
+}
+
+# Usage example:
+# To call this function without filter
+# select_vf_dpdk
+
+# To call this function with a bus filter, e.g., 0000:03
+select_vf_dpdk 0000:03
