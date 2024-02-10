272e53602bebac1ee4454f932880b4257bc6c170
spyroot <spyroot@gmail.com>
2024-02-10 14:37:41 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/run_pkt_gen.sh
+++ pktgen_toolbox_generic/scripts/run_pkt_gen.sh
@@ -1,8 +1,90 @@
+#!/bin/bash
+# Set of utility function to work with NUMA , DPDK and pktgen
+#
+# Autor Mus spyroot@gmail.com
+
+# Select vf based on PMD driver and PMD BUS
+# Note this function uses container ( you change to dpdk-devbind.py
+function select_vf_dpdk {
+    local DPDK_PMD_TYPE="vfio-pci"
+    local BUS_FILTER="$1"
+
+    local output="$(docker run -it --privileged --rm \
+                    spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep $DPDK_PMD_TYPE | grep Virtual)"
+
+    local pci_devices
+    readarray -t pci_devices <<< "$(echo "$output" | awk \
+    '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
+
+    for pci in "${pci_devices[@]}"; do
+        if [[ -n "$BUS_FILTER" ]]; then
+            if [[ "$pci" == "$BUS_FILTER"* ]]; then
+                echo "$pci"
+            fi
+        else
+            # If no bus filter is provided, show all VFs.
+            echo "$pci"
+        fi
+    done
+}
+
+
+# This function retrieves a list of all CPU cores that
+# are currently bound to the system's physical CPUs.
+core_list() {
+	core_list_str=$(numactl -s | grep "physcpubind:" | sed 's/physcpubind: //')
+	read -r -a core_list <<< "$core_list_str"
+	echo "${core_list[@]}"
+}
+
+
+# This function selects a specified number
+# of random CPU cores from a given NUMA node.
+# Args:
+#   $1: The NUMA node from which to select cores.
+#   $2: The number of cores to select from that NUMA node.
+cores_from_numa(){
+	local _numa_node=$1
+	local _num_cores_to_select=$2
+	local cpu_list_str=$(numactl -H | grep -E "^node $_numa_node cpus:" | cut -d: -f2)
+	local core_list=()
+	read -r -a core_list <<< "$cpu_list_str" # Convert string to array
+
+    # if core is less than or equal to available cores
+    if [ "${#core_list[@]}" -lt "$_num_cores_to_select" ]; then
+	    echo "Error: Requested more cores than available." >&2
+	    return 1
+    fi
+
+    local selected_cores=()
+    # Select random cores
+    for i in $(shuf -i 0-$((${#core_list[@]}-1)) -n "$_num_cores_to_select"); do
+	    selected_cores+=("${core_list[$i]}")
+    done
+
+    echo "${selected_cores[@]}"
+}
+
+# from numa 0, select 4 cores at random
+numa_node=0
+num_cores_to_select=4
+
+selected_cores=$(cores_from_numa $numa_node $num_cores_to_select)
+echo "cores from node $numa_node:"
+for cpu in $selected_cores; do echo "$cpu"
+done
+
+core_list=$(core_list)
+
+echo "available cores:"
+for core in "${core_list[@]}"; do echo "$core"
+done
+
 docker run \
--e BUFFER_SIZE="8 16" \
--e STRIDE="24 32" \
--e CORES="2-3" \
--e CONSOLE_OUT="true" \
+-e SELECTED_CORES=""$selected_cores"" \
+-e TARGET_VFS="$selected_vfs"
 -it --privileged --rm \
-spyroot/pktgen_toolbox_generic:latest:latest pktgen \
--l 2-14 -n 4 --proc-type auto --log-level 7 --file-prefix pg -a 0000:23:02.0 -- -T -m "[4-7:10-13].0"
+spyroot/pktgen_toolbox_generic:latest:latest run_pktgen.sh
+
+#pktgen \
+#-l 2-14 -n 4 --proc-type auto --log-level 7 --file-prefix pg -a 0000:23:02.0 -- -T -m "[4-7:10-13].0"
--- pktgen_toolbox_generic/scripts/select_cores.sh
+++ pktgen_toolbox_generic/scripts/select_cores.sh
@@ -1,27 +1,82 @@
 #!/bin/bash
+# set of utility function to work with NUMA , DPDK.
+#
+# Autor Mus spyroot@gmail.com
 
-select_random_cores() {
-    local -n _core_list=$1
-    local _num_cores=$2
-    local _selected_cores=()
+# Select vf based on PMD driver and PMD BUS
+# Note this function uses container ( you change to dpdk-devbind.py
+function select_vf_dpdk {
+    local DPDK_PMD_TYPE="vfio-pci"
+    local BUS_FILTER="$1"
 
-    # shuffle the array and pick the first N elements
-    _selected_cores=($(shuf -e "${_core_list[@]}" -n $_num_cores))
+    local output="$(docker run -it --privileged --rm \
+                    spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep $DPDK_PMD_TYPE | grep Virtual)"
 
-    echo "${_selected_cores[*]}"
+    local pci_devices
+    readarray -t pci_devices <<< "$(echo "$output" | awk \
+    '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
+
+    for pci in "${pci_devices[@]}"; do
+        if [[ -n "$BUS_FILTER" ]]; then
+            if [[ "$pci" == "$BUS_FILTER"* ]]; then
+                echo "$pci"
+            fi
+        else
+            # If no bus filter is provided, show all VFs.
+            echo "$pci"
+        fi
+    done
 }
 
-# Get the list of all available cores from numactl
-core_list_str=$(numactl -s | grep "physcpubind:" | sed 's/physcpubind: //')
-read -r -a core_list <<< "$core_list_str" # Convert string to array
 
-# Define NUMA nodes you're interested in
-numa_nodes=("0" "1" "2" "3")
+# This function retrieves a list of all CPU cores that
+# are currently bound to the system's physical CPUs.
+core_list() {
+	core_list_str=$(numactl -s | grep "physcpubind:" | sed 's/physcpubind: //')
+	read -r -a core_list <<< "$core_list_str"
+	echo "${core_list[@]}"
+}
 
-# Define how many cores you want to select randomly
+
+# This function selects a specified number
+# of random CPU cores from a given NUMA node.
+# Args:
+#   $1: The NUMA node from which to select cores.
+#   $2: The number of cores to select from that NUMA node.
+cores_from_numa(){
+	local _numa_node=$1
+	local _num_cores_to_select=$2
+	local cpu_list_str=$(numactl -H | grep -E "^node $_numa_node cpus:" | cut -d: -f2)
+	local core_list=()
+	read -r -a core_list <<< "$cpu_list_str" # Convert string to array
+
+    # if core is less than or equal to available cores
+    if [ "${#core_list[@]}" -lt "$_num_cores_to_select" ]; then
+	    echo "Error: Requested more cores than available." >&2
+	    return 1
+    fi
+
+    local selected_cores=()
+    # Select random cores
+    for i in $(shuf -i 0-$((${#core_list[@]}-1)) -n "$_num_cores_to_select"); do
+	    selected_cores+=("${core_list[$i]}")
+    done
+
+    echo "${selected_cores[@]}"
+}
+
+# from numa 0, select 4 cores at random
+numa_node=0
 num_cores_to_select=4
+selected_cores=$(cores_from_numa $numa_node $num_cores_to_select)
 
-# Call the function to select random cores
-selected_cores=$(select_random_cores core_list num_cores_to_select)
+echo "cores from node $numa_node:"
+for cpu in $selected_cores; do echo "$cpu"
+done
 
-echo "Selected cores: $selected_cores"
+core_list=$(core_list)
+
+echo "available cores:"
+for core in "${core_list[@]}"; do echo "$core"
+done
+
