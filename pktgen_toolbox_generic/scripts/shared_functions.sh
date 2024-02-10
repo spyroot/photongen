316cfc1ac04d76091bbd4d237fc393955bc4664c
spyroot <spyroot@gmail.com>
2024-02-11 02:13:58 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/shared_functions.sh
+++ pktgen_toolbox_generic/scripts/shared_functions.sh
@@ -10,20 +10,12 @@ function vf_mac_address() {
         return
     fi
 
-    # Validate full PCI address format (0000:XX:XX.X)
-    if [[ $_pci_address =~ ^[0-9a-fA-F]{4}:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}\.[0-9a-fA-F]{1}$ ]]; then
-        _mac_address=$(dmesg | grep -E "${_pci_address}(\.[0-9a-fA-F]{2}){2}\b" | \
-        grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
-    # Validate partial PCI address format (XX:XX.X)
-    elif [[ $_pci_address =~ ^[0-9a-fA-F]{2}:[0-9a-fA-F]{2}\.[0-9a-fA-F]{1}$ ]]; then
-        _pci_address="0000:$_pci_address"
-        _mac_address=$(dmesg | grep -E "${_pci_address}(\.[0-9a-fA-F]{2}){2}\b" | \
-        grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
-    else
+    # Check if the PCI address matches the expected formats
+    if [[ $_pci_address =~ ^0000:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:\.[0-9a-fA-F]{1,2}$ ]]; then
+          _mac_address=$(dmesg | grep "$_pci_address" | grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
+          echo "$_mac_address"
+    elif [[ ! $_pci_address =~ ^0000:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:\.[0-9a-fA-F]{1,2}$ ]]; then
         echo "Invalid PCI address format: $_pci_address"
         return 1
     fi
-
-    echo "$_mac_address"
-
 }
