708452e50a6dd957f334568a4648f8c099114ffd
spyroot <spyroot@gmail.com>
2024-02-11 02:15:37 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/shared_functions.sh
+++ pktgen_toolbox_generic/scripts/shared_functions.sh
@@ -1,20 +1,14 @@
-# Function return mac address of device
-# note if device already bounded to DPDK this is most reliable way to get it
+# Function to return mac address of device
+# Note: If the device is already bound to DPDK, this is the most reliable way to get its MAC address
 function vf_mac_address() {
     local _pci_address=$1
     local _mac_address
 
-    if [ -z "$_pci_address" ]; then
-        # Return an empty string
-        echo ""
-        return
-    fi
-
-    # Check if the PCI address matches the expected formats
-    if [[ $_pci_address =~ ^0000:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:\.[0-9a-fA-F]{1,2}$ ]]; then
-          _mac_address=$(dmesg | grep "$_pci_address" | grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
-          echo "$_mac_address"
-    elif [[ ! $_pci_address =~ ^0000:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}:\.[0-9a-fA-F]{1,2}$ ]]; then
+    # Check if the PCI address matches the expected format (0000:XX:XX.X)
+    if [[ $_pci_address =~ ^0000:[0-9a-fA-F]{2}:[0-9a-fA-F]{2}\.[0-9a-fA-F]{1}$ ]]; then
+        _mac_address=$(dmesg | grep "$_pci_address" | grep 'MAC' | awk '{print $NF}' | grep -Eo '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}' | tail -n 1)
+        echo "$_mac_address"
+    else
         echo "Invalid PCI address format: $_pci_address"
         return 1
     fi
