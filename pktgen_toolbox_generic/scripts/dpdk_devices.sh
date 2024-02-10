484a0b45d4f46071e420d105d4ee1453d1bdac56
spyroot <spyroot@gmail.com>
2024-02-10 13:30:50 +0400

    add generic build

--- pktgen_toolbox_generic/scripts/dpdk_devices.sh
+++ pktgen_toolbox_generic/scripts/dpdk_devices.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # return list of all PCI DPDK devices
-output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep vfio-pci | grep Virtual)"
+output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s)"
 readarray -t pci_devices <<< "$(echo "$output" | awk '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
 
 for pci in "${pci_devices[@]}"; do
--- pktgen_toolbox_generic/scripts/select_vf.sh
+++ pktgen_toolbox_generic/scripts/select_vf.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# return list of all PCI DPDK devices
-output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep vfio-pci | grep Virtual)"
+DPDK_PMD_TYPE="vfio-pci"
+
+# select vf DPDK cable
+output="$(docker run -it --privileged --rm spyroot/pktgen_toolbox_generic:latest dpdk-devbind.py -s | grep $DPDK_PMD_TYPE | grep Virtual)"
 readarray -t pci_devices <<< "$(echo "$output" | awk '/^[0-9a-f]{4}:[0-9a-f]{2}:[0-9a-f]{2}\.[0-9a-f]/ {print $1}')"
 
 for pci in "${pci_devices[@]}"; do
