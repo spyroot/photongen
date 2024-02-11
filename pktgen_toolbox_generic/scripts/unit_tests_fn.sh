838daef212d2843a6c580da8afcacfd3ce4cf9c2
spyroot <spyroot@gmail.com>
2024-02-11 04:24:12 +0400

    add selection of vf

--- pktgen_toolbox_generic/scripts/unit_tests_fn.sh
+++ pktgen_toolbox_generic/scripts/unit_tests_fn.sh
@@ -209,6 +209,35 @@ function test_validate_numa() {
     fi
 }
 
+# test list of core note it platform specific
+#
+function test_all_cores_from_numa {
+    local test_passed=true
+
+    local expected_cores=(
+        "0 1 2 3 4 5 6 7 8 9 10 11 48 49 50 51 52 53 54 55 56 57 58 59"
+        "12 13 14 15 16 17 18 19 20 21 22 23 60 61 62 63 64 65 66 67 68 69 70 71"
+        "24 25 26 27 28 29 30 31 32 33 34 35 72 73 74 75 76 77 78 79 80 81 82 83"
+        "36 37 38 39 40 41 42 43 44 45 46 47 84 85 86 87 88 89 90 91 92 93 94 95"
+    )
+
+    for numa_node in {0..3}; do
+        local expected=${expected_cores[$numa_node]}
+        local actual=$(get_cores_for_numa "$numa_node")
+        if [ "$actual" != "$expected" ]; then
+            echo "test_all_cores_from_numa failed: Expected cores $expected for NUMA node $numa_node but got $actual"
+            test_passed=false
+        fi
+    done
+
+    if [ "$test_passed" = true ]; then
+        echo "test_all_cores_from_numa passed: All tests passed successfully"
+    else
+        echo "test_all_cores_from_numa failed: Some tests failed"
+    fi
+
+}
+
 function test_cores_from_numa() {
 
     local test_passed=true
@@ -245,8 +274,6 @@ function test_cores_from_numa() {
 test_vf_mac_address
 test_adapter_numa
 test_validate_numa
-test_cores_from_numa
+test_all_cores_from_numa
 
-numa_node=0
-cores=$(get_cores_for_numa "$numa_node")
-
+#test_cores_from_numa
